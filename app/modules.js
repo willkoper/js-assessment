763cd7fe020c2e2f573648a867cb8bb6eb8efe20
Will Koper <koperw@washpost.com>
2012-10-25 00:15:31 -0400

    modules, best practices, some regex

--- app/modules.js
+++ app/modules.js
@@ -3,7 +3,15 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     createModule : function(str1, str2) {
-
+    	var greeting = str1,
+    		name = str2,
+    		sayIt = function(){
+					return this.greeting +', '+ this.name};
+		return {
+				greeting:greeting,
+				name: name,
+				sayIt: sayIt 
+			}
     }
   };
 });
