847c2fe7984ca5d694bfc1b07f8e4c61944228a3
Will Koper <koperw@washpost.com>
2012-10-26 14:14:54 -0400

    passes controlFlow

--- app/flowControl.js
+++ app/flowControl.js
@@ -4,13 +4,29 @@ define(function() {
   return {
     fizzBuzz : function(num) {
       // write a function that receives a number as its argument;
+      var output = '';
+      if(typeof num !== 'number'){
+	return false;
+      }
+      if(num%3 === 0){
       // if the number is divisible by 3, the function should return 'fizz';
+	output += 'fizz';
+      }
+      if(num%5 === 0){
       // if the number is divisible by 5, the function should return 'buzz';
+	output += 'buzz';
+      }
       // if the number is divisible by 3 and 5, the function should return
       // 'fizzbuzz';
       //
       // otherwise the function should return the number, or false if no number
       // was provided or the value provided is not a number
+      if(output === ''){
+	// otherwise the function should return the number, or false if no number
+      // was provided
+	output = num;
+      }
+      return output;
     }
   };
 });
