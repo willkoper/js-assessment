a1d2c5e5bdc44a126335d7bba247678952122648
Will Koper <koperw@washpost.com>
2012-10-26 12:14:04 -0400

    logical operators finished

--- app/functions.js
+++ app/functions.js
@@ -3,7 +3,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+		return fn(arr);
     },
 
     speak : function(fn, obj) {
