94acae985132e20334c661992980f3fc8e15761a
Will Koper <koperw@washpost.com>
2012-10-29 15:33:15 -0400

    objects passes

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+		return fn.call(obj, null);
     },
 
     alterObjects : function(constructor, greeting) {
-
+		constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+		var output = [],
+			prop
+		for (prop in obj){
+			if(obj.hasOwnProperty(prop)){
+			output.push(prop+': '+obj[prop]);
+			}
+		}
+		return output;
     }
   };
 });
