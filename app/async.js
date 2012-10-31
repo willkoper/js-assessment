160f9034c3f0f88016cbb6ec72f384370c5c2e0c
Will Koper <koperw@washpost.com>
2012-10-31 10:44:18 -0400

    Cheated on async

--- app/async.js
+++ app/async.js
@@ -1,9 +1,11 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
-
+//http://joseoncode.com/2011/09/26/a-walkthrough-jquery-deferred-and-promise/
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+		var d = $.Deferred();
+		setTimeout(function(){d.resolve(value)}, 20);
+		return d.promise();	
     },
 
     manipulateRemoteData : function(url) {
